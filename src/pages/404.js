e807145e155f339a4d64ae2952f101bf4f7a8081
kafierence <vannam34760@gmail.com>
2023-06-13 10:37:36 +0700

    Rebuild About Us Translate

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,6 +4,7 @@ import Head from 'next/head';
 import Image from 'next/image';
 
 const NotFound = () => {
+  const { t } = useTranslation()
   return (
     <>
       <Head>
@@ -15,15 +16,15 @@ const NotFound = () => {
         <div className="block justify-items-stretch mx-auto items-center text-center">
           <Image width={650} height={450} src="/404.svg" alt="logo" />
           <h2 className="font-bold font-serif font-2xl lg:text-4xl leading-6 mb-4">
-            Page is not found!
+            {t("common:Page-Not-Found-Title")}
           </h2>
           <p className="block text-center text-base font-sans text-gray-600">
-            Sorry! This page is not found! Please try again later.
+            {t("Page-Not-Found-Description")}
           </p>
 
           <Link href="/">
             <a className="md:text-sm leading-4 inline-flex items-center cursor-pointer transition ease-in-out duration-300 font-semibold text-center justify-center border-0 border-transparent rounded-md placeholder-white focus-visible:outline-none focus:outline-none bg-emerald-500 text-white px-5 md:px-6 lg:px-8 py-3 md:py-3.5 lg:py-3 hover:text-white hover:bg-emerald-600 h-12 mt-6 text-sm lg:text-base w-full sm:w-auto">
-              Back to Home
+              {t("Back-To-Home")}
             </a>
           </Link>
         </div>
