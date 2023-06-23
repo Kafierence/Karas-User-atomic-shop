b3baf6727fbe958536b2074c4e58f87922e790eb
kafierence <vannam34760@gmail.com>
2023-06-23 10:21:51 +0700

    Add Product Type Two

---
+++ src/component/product/ProductCardSecond.js
@@ -0,0 +1,10 @@
+import React from 'react'
+// Setting Product Type Two
+const ProductCardSecond = () => {
+
+    return (
+        <div>ProductCardSecond</div>
+    )
+}
+
+export default ProductCardSecond
