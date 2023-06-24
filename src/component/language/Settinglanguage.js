f9545ce6f248d71207d75720429265e673687888
Kafierence <vannam34760@gmail.com>
2023-06-24 15:27:09 +0700

    Fix the env

--- src/component/language/Settinglanguage.js
+++ src/component/language/Settinglanguage.js
@@ -39,19 +39,20 @@ const Settinglanguage = () => {
                     &nbsp;<i className="fas fa-angle-down"></i>
                 </button>
                 <Box className="dropdown-content" p={4} gap={2}>
-                    {data.map((language) =>
+                    {data.map((language, index) =>
                     (
                         <>
                             {
                                 language.status !== "hide" &&
                                 <Link
                                     as={NextLink}
-                                    key={language.iso_code}
+                                    key={`${index} ${language.iso_code}`}
                                     href="/"
                                     locale={`${language.iso_code}`}
                                 >
 
-                                    <Box _hover={{ color: 'red' }} cursor="pointer" py={2} onClick={() => setCurrentLang(language)}>
+                                    <Box _hover={{ color: 'red' }}
+                                        cursor="pointer" py={2} onClick={() => setCurrentLang(language)}>
                                         <Box
                                             className={`flot-l flag ${language?.flag?.toLowerCase()}`} />
 
